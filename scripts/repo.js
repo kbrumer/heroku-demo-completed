87ece2fc38aeb276dc4b8b192a5e28e8a633a8bd
Ken Brumer <kbrumer@sosacorp.com>
2016-06-11 12:00:58 -0700

    Fixing

--- scripts/repo.js
+++ scripts/repo.js
@@ -4,17 +4,20 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    $.ajax({
-      url: 'https://api.github.com/users/kbrumer/repos' +
-            '?per_page=100' +
-            '&sort=updated',
-      type: 'GET',
-      headers: { 'Authorization': 'token ' + githubToken },
-      success: function(data, message, xhr) {
-        repos.all = data;
-      }
-    }).then(callback);
+    // $.ajax({
+    //   url: 'https://api.github.com/users/kbrumer/repos' +
+    //         '?per_page=100' +
+    //         '&sort=updated',
+    //   type: 'GET',
+    //   headers: { 'Authorization': 'token ' + githubToken },
+    //   success: function(data, message, xhr) {
+    //     repos.all = data;
+    //   }
+    // }).then(callback);
+    console.log('** repo.requestRepos called **');
+
   };
 
+  console.log('** repo called **');
   module.repos = repos;
 })(window);
